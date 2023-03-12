ee1159cc4891b74f77bff25ffcaa697b21d3ae19
Sunlyc1 <127405755+Sunlyc1@users.noreply.github.com>
2023-03-12 22:48:09 +0800

    Update duoduovip.js

--- duoduovip.js
+++ duoduovip.js
@@ -1,9 +1,7 @@
 var body = $response.body; 
 var obj = JSON.parse(body); 
 
-obj.data.user.vipInfo.level = 7
 obj.data.user.vipInfo.valid = true
-obj.data.user.vipInfo.expiredTime =  1776307200
 
 body = JSON.stringify(obj);
 $done({body});
